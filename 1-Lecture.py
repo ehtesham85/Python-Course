815f9acac667401fd031a3dd7fbc57d4454ef8d6
Ehtesham Jutt <ehtesham8586@gmail.com>
2025-07-19 11:20:20 +0500

    Add files via upload

---
+++ 1-Lecture.py
@@ -0,0 +1,50 @@
+#Basic variables/Data types
+# int 1,45,67
+# float 2.4, 45.2
+# string "Ali", "Hello"
+# boolean
+# list
+# tuple
+# set
+# dict
+
+
+
+# in C++
+# include <iostream>
+# int main() {
+#     std::cout << "Hello, World!" << std::endl;
+#     return 0;
+# }
+
+# in Java
+# public class HelloWorld {
+#     public static void main(String[] args) {
+#         System.out.println("Hello, World!");
+#     }
+# }
+
+print("Hello")
+# a=10
+# b=12.1
+# c="Hello"
+# print(a)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print("Hello how are you")
+#
+# name="Ali"
+# age="21"
+# print("Name is",name)
+# print(name,"age is",age)
+#
+# name=input("Enter a name:")
+# age=input("Enter your Age:")
+# qual=input("Enter your Qualification:")
+#
+# print(name)
+# print(age)
+# print(qual)
+#
+# print("Your Name is:",name,"Your Age is:",age,"Your Qualification is:",qual)
