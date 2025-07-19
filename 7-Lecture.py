e8f4b85115cac121725827229e8edc9f22164988
Ehtesham Jutt <ehtesham8586@gmail.com>
2025-07-19 11:22:37 +0500

    Add files via upload

---
+++ 7-Lecture.py
@@ -0,0 +1,59 @@
+# ARITHMETIC  +, -
+# ASSIGNMENT  =, ==, +=, -=
+# RELATIONAL  >,<
+# BOOLEAN     true, false
+# LOGICAL     and, or, not
+
+# num1=float(input("Enter a Number 1:"))
+# num2=float(input("Enter a Number 2:"))
+# num3=float(input("Enter a Number 3:"))
+#
+# sum=num1+num2+num3
+
+# print("Sum of Number =",sum)
+# print(f"Sum of {num1} + {num2} + {num3} = ",sum)
+#
+# print(num1 > num2)
+# print(num1 > 10 and num2 < 14 )
+# print(not(num1 > num2))
+# print(num1 == num2)
+# print(num1 < num2)
+#
+# if num1 == num2:
+#     print("EQUAL")
+# else:
+#     print("Not Equal")
+
+import math
+
+
+print("Value of pi = ",math.pi)
+
+print("Square root = ",math.sqrt(16))
+
+print("Power of Number = ", math.pow(3,2))
+
+print("Floor of Number ",math.floor(5.8))
+
+print("Ceiling of Number ",math.ceil(5.3))
+
+print("Log of Number",math.log(10))
+
+math.cos()
+math.sin()
+math.tan()
+math.e
+math.exp()
+math.log10()
+math.log2()
+math.fabs()
+math.factorial()
+math.sumprod()
+math.degrees()
+math.radians()
+sum()
+max()
+min()
+
+
+
