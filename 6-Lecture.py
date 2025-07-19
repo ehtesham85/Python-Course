d6abb0acbd41d0c1e062fc7aad2ec09704494d1e
Ehtesham Jutt <ehtesham8586@gmail.com>
2025-07-19 11:22:16 +0500

    Add files via upload

---
+++ 6-Lecture.py
@@ -0,0 +1,70 @@
+
+
+# a=10
+# print(type(a))
+#
+# b=2.3
+# print(type(b))
+#
+# c="Ali"
+# print(type(c))
+
+# name="Ali"
+# age="20"
+# grad="Inter"
+#
+# print("My name is:",name,"My age is:",age,"I am in",grad,"class")
+
+# name=str(input("Enter your Name:"))
+# age=int(input("Enter your Age:"))
+# qual=str(input("Enter your Qualification:"))
+#
+# print("My name is:",name,", My age is:",age,", I am in",qual,"class")
+
+
+# num1=float(input("Enter a Number:"))
+# num2=float(input("Enter a Number:"))
+#
+# sum=num1 + num2
+# mul=num1 * num2
+#
+# print("Sum of Number is: ",sum)
+# print("Mul of Number is: ",mul)
+#
+# print(f"Addition of {num1} + {num2} =", sum)
+# print(f"Multiplication of {num1} * {num2} =",mul)
+#
+# print(f"Addition of {num1} + {num2} = {sum}")
+# print(f"Multiplication of {num1} * {num2} = {mul}")
+
+# base=float(input("Enter a Length of Base:"))
+# height=float(input("Enter a Length of Height:"))
+#
+# area=0.5 * base * height
+# print(f"Area of Triangle is:{area}")
+
+
+# a=float(input("Enter a Number:"))
+# b=float(input("Enter a Number:"))
+#
+# print(f"Original value of a is {a} and b is {b}")
+#
+# temp = a
+# a = b
+# b = temp
+#
+# print(f"Swapping value of a is {a} and b is {b}")
+
+import random
+print(f"Random number is: {random.randint(1,10)}")
+
+
+# Practice do your self
+#
+# program to covert mile into kilometer
+# program to convert days into hours into minutes into seconds
+
+
+
+
+
